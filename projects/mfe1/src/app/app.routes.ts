eaef30b84efc1d4329fbd90acf366af47697c54b
Eugene Kruglei <eugene.kruglei@gmail.com>
2022-10-26 16:23:53 +0300

    Removed route redirect.

--- projects/mfe1/src/app/app.routes.ts
+++ projects/mfe1/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { FlightsSearchComponent } from './flights/flights-search/flights-search.component';
 
 export const APP_ROUTES: Routes = [
-    { path: '*', redirectTo: 'flights-search' },
     { path: '', component: HomeComponent, pathMatch: 'full'},
     { path: 'flights-search', component: FlightsSearchComponent},
 ];
