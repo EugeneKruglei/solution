a8cef4359179299e75fbaef3fd304dd5fd320b8d
Eugene Kruglei <eugene.kruglei@gmail.com>
2022-10-26 16:21:10 +0300

    Reverted webpack configs.

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,4 +1,4 @@
-const {share, shareAll, withModuleFederationPlugin} = require('@angular-architects/module-federation/webpack');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -8,24 +8,8 @@ module.exports = withModuleFederationPlugin({
         './Module': './projects/mfe1/src/app/app.module.ts',
     },
 
-    shared:
-        share(
-            {
-                '@angular/core': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/common': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/common/http': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/router': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/forms': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/platform-browser': {
-                    singleton: true,
-                    strictVersion: false,
-                    requiredVersion: 'auto',
-                    eager: true,
-                    import: 'node_modules/@angular/platform-browser'},
-                '@angular/platform-browser/animations': {
-                    singleton: true,
-                    import: 'node_modules/@angular/platform-browser/animations'
-                },
-            })
+    shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    },
 
 });
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,24 +1,9 @@
-const {withModuleFederationPlugin, share} = require('@angular-architects/module-federation/webpack');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
-    shared:
-        share(
-            {
-                '@angular/core': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/common': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/common/http': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/router': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/forms': {singleton: true, strictVersion: true, requiredVersion: 'auto'},
-                '@angular/platform-browser': {
-                    singleton: true,
-                    strictVersion: false,
-                    requiredVersion: 'auto',
-                    eager: true,
-                    import: 'node_modules/@angular/platform-browser'},
-                '@angular/platform-browser/animations': {
-                    singleton: true,
-                    import: 'node_modules/@angular/platform-browser/animations'
-                },
-            })
+    shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    },
+
 });
